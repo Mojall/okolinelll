45ec2ed69ee3d49964f5de8592b9c0b3aab6656c
Mojall <astemirrros@gmail.com>
2024-04-29 16:01:13 +0300

    kekelandiya

--- src/pages/AccountStatus.vue
+++ src/pages/AccountStatus.vue
@@ -2,20 +2,20 @@
     <div class="container">
         <h1>Информация о сервере</h1>
         <div class="tariff-info">
-            <h2>Текущий тариф: {{ getTariffName(userTariff.name) }}</h2>
+            <h2>Текущий тариф: {{ selectedTariff.name }}</h2>
         </div>
         <ul class="hardware-info-list">
-            <li v-if="getTariffDescription(userTariff.name).cpu !== null" class="hardware-info-item">
-                CPU: {{ getTariffDescription(userTariff.name).cpu }}
+            <li v-if="selectedTariffSpecs.cpuCoresCount !== null" class="hardware-info-item">
+                CPU: {{ selectedTariffSpecs.cpuCoresCount }} {{ pluralize(selectedTariffSpecs.cpuCoresCount, 'ядро', 'ядра', 'ядер') }}
             </li>
-            <li v-if="getTariffDescription(userTariff.name).ram !== null" class="hardware-info-item">
-                RAM: {{ getTariffDescription(userTariff.name).ram }}
+            <li v-if="selectedTariffSpecs.ramInGb !== null" class="hardware-info-item">
+                RAM: {{ selectedTariffSpecs.ramInGb }} Гб
             </li>
-            <li v-if="getTariffDescription(userTariff.name).ssd !== null" class="hardware-info-item">
-                SDD: {{ getTariffDescription(userTariff.name).ssd }}
+            <li v-if="selectedTariffSpecs.ssdInGb !== null" class="hardware-info-item">
+                SDD: {{ selectedTariffSpecs.ssdInGb }} Гб
             </li>
-            <li v-if="getTariffDescription(userTariff.name).ip !== null" class="hardware-info-item">
-                IP: {{ getTariffDescription(userTariff.name).ip }}
+            <li v-if="selectedTariffSpecs.ipCount !== null" class="hardware-info-item">
+                IP: {{ selectedTariffSpecs.ipCount }}
             </li>
         </ul>
         <div class="hardware-info-item">
@@ -56,8 +56,8 @@
 <!--                    CPU: {{ tariff.specs.cpuCoresCount }}-->
 <!--                    {{ pluralize(tariff.specs.cpuCoresCount, 'ядро', 'ядра', 'ядер') }}-->
 <!--                </p>-->
-<!--                <p v-if="tariff.specs.hardInGb !== null">-->
-<!--                    HDD: {{ tariff.specs.hardInGb }} Гб-->
+<!--                <p v-if="tariff.specs.ramInGb !== null">-->
+<!--                    RAM: {{ tariff.specs.ramInGb }} Гб-->
 <!--                </p>-->
 <!--                <p v-if="tariff.specs.ssdInGb !== null">-->
 <!--                    SDD: {{ tariff.specs.ssdInGb }} Гб-->
@@ -73,15 +73,20 @@ import { refreshAxios } from '../api/api.js';
 import { onMounted, ref } from 'vue';
 import Cookies from 'js-cookie';
 import { jwtDecode } from 'jwt-decode';
-import { pluralize, getTariffName, getTariffDescription } from '../utils/utils.js';
+import { pluralize, formatAmount } from '../utils/utils.js';
 
 const tariffs = ref([]);
 const isLoading = ref(false);
+const selectedTariff = ref({});
+const selectedTariffSpecs = ref({});
+const selectedTariffName = ref('');
 
 const token = Cookies.get('jwtToken');
 const decodeToken = jwtDecode(token);
 const { tariff_data } = decodeToken;
 const userTariff = tariff_data.specs;
+const userTariffName = tariff_data.specs.name;
+
 
 
 onMounted(async () => {
@@ -95,11 +100,17 @@ onMounted(async () => {
         });
         const data = response.data;
         tariffs.value = data['hydra:member'];
+        selectedTariff.value = tariffs.value.find(tariff => tariff.specs.name === userTariffName);
+        if (selectedTariff.value) {
+            selectedTariffSpecs.value = selectedTariff.value.specs;
+            selectedTariffName.value = selectedTariff.value.name;
+        }
         isLoading.value = false;
     } catch (error) {
         console.log('Ошибка при получении списка тарифов:', error);
     }
 });
+
 </script>
 
 <style lang="sass" scoped>
